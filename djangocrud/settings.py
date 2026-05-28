9dbf9a0e12d73cf0b05982089411a2696bdeaf86
Jose <22380597@itcv.edu.mx>
2026-05-27 18:23:30 -0600

    Esperemos ya sea el bueno2

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+^qqt_n8mms)qi_usiwu%e9_eyqanw=bx29dsd=&52+@2tp3^%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
